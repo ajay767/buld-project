95eddb95279cb8d169a250c0a230d6ee9e0eefa9
AJAY YADAV <58135870+ajay767@users.noreply.github.com>
2020-05-18 11:53:20 +0530

    uploaded the files

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector('.on').addEventListener('click', () =>{
+document.querySelector('.img-box img').setAttribute('src' , 'on.png')
+});
+document.querySelector('.off').addEventListener('click', () =>{
+    document.querySelector('.img-box img').setAttribute('src' , 'off.png')
+    });
